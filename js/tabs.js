6fcaed5e487e76ab813abc3afd4a77ab32cddd7f
michael johansson <micke@mickejohansson.info>
2018-04-16 10:24:05 +0200

    Add navigation that doesn't work

---
+++ js/tabs.js
@@ -0,0 +1,9 @@
+$('#projectlink').click(() => {
+  $('#content-home').css('display', 'none')
+  $('#content-project').css('display', 'block')
+})
+
+$('#homelink').click(() => {
+  $('#content-project').css('display', 'none')
+  $('#content-home').css('display', 'block')
+})

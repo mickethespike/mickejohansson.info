336c9020f28a93fcf8582e81835835581780a9c0
michael johansson <micke@mickejohansson.info>
2018-04-17 14:11:45 +0200

    add monster hunter tab - removed mobile support

--- js/tabs.js
+++ js/tabs.js
@@ -1,9 +1,17 @@
 $('#projectlink').click(() => {
   $('#content-home').css('display', 'none')
+  $('#content-monster-hunter').css('display', 'none')
   $('#content-project').css('display', 'flex')
 })
 
 $('#homelink').click(() => {
   $('#content-project').css('display', 'none')
+  $('#content-monster-hunter').css('display', 'none')
   $('#content-home').css('display', 'flex')
+})
+
+$('#monsterlink').click(() => {
+  $('#content-project').css('display', 'none')
+  $('#content-home').css('display', 'none')
+  $('#content-monster-hunter').css('display', 'flex')
 })

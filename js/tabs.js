aed196c0cad0a4eb6861e306d43afb01560d87a9
michael johansson <micke@mickejohansson.info>
2018-04-18 10:06:42 +0200

    Fix naming error

--- js/tabs.js
+++ js/tabs.js
@@ -1,17 +1,17 @@
 $('#projectlink').click(() => {
   $('#content-home').css('display', 'none')
-  $('#content-monster-hunter').css('display', 'none')
+  $('#content-monster-slaught').css('display', 'none')
   $('#content-project').css('display', 'flex')
 })
 
 $('#homelink').click(() => {
   $('#content-project').css('display', 'none')
-  $('#content-monster-hunter').css('display', 'none')
+  $('#content-monster-slaught').css('display', 'none')
   $('#content-home').css('display', 'flex')
 })
 
 $('#monsterlink').click(() => {
   $('#content-project').css('display', 'none')
   $('#content-home').css('display', 'none')
-  $('#content-monster-hunter').css('display', 'flex')
+  $('#content-monster-slaught').css('display', 'flex')
 })

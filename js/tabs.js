8857d02f2b720faa55ef28cf0c39c42e58aa4fa9
michael johansson <micke@mickejohansson.info>
2018-04-19 11:12:10 +0200

    Add todo tab

--- js/tabs.js
+++ js/tabs.js
@@ -1,17 +1,28 @@
+
 $('#projectlink').click(() => {
   $('#content-home').css('display', 'none')
   $('#content-monster-slaught').css('display', 'none')
+  $('#content-todo').css('display', 'none')
   $('#content-project').css('display', 'flex')
 })
 
 $('#homelink').click(() => {
   $('#content-project').css('display', 'none')
   $('#content-monster-slaught').css('display', 'none')
+  $('#content-todo').css('display', 'none')
   $('#content-home').css('display', 'flex')
 })
 
 $('#monsterlink').click(() => {
   $('#content-project').css('display', 'none')
   $('#content-home').css('display', 'none')
+  $('#content-todo').css('display', 'none')
   $('#content-monster-slaught').css('display', 'flex')
+})
+
+$('#todolink').click(() => {
+  $('#content-project').css('display', 'none')
+  $('#content-home').css('display', 'none')
+  $('#content-monster-slaught').css('display', 'none')
+  $('#content-todo').css('display', 'flex')
 })
